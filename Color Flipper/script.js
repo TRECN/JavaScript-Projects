0f9c0dee90a7132188dc6a25327f1d421e5bd44d
TRECN <rishabh1103@outlook.com>
2021-07-18 22:04:06 +0530

    commit

--- Color Flipper/script.js
+++ Color Flipper/script.js
@@ -1,6 +1,9 @@
+const colors=["green","red", "gray", "blue"];
 const btn=document.getElementById('btn');
 const color=document.getElementById('color');
 btn.addEventListener("click", 
 function (){
+    color.textContent=colors[1];
     document.body.style.backgroundColor=color.textContent;
+    
 });
