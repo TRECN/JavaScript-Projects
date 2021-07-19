613ffd3939968c7ebf8bbe3d10b8bea722280c78
TRECN <rishabh1103@outlook.com>
2021-07-19 23:09:25 +0530

    commit

--- Counter/script.js
+++ Counter/script.js
@@ -1,5 +1,6 @@
 let value=0;
 const val=document.getElementById('value');
+const btns=document.querySelectorAll('.btn');
 const decrease=document.getElementById('decrease');
 const increase=document.getElementById('increase');
 const reset= document.getElementById('reset');
@@ -7,14 +8,26 @@ const reset= document.getElementById('reset');
 decrease.addEventListener("click",
 function(){
     value=value-1;
-    val.textContent=value;
 });
 increase.addEventListener("click",function(){
-    value=value+1;
-    val.textContent=value;
-    
+    value=value+1;   
 });
 reset.addEventListener("click",function(){
     value=0;
-    val.textContent=value; 
+   
+});
+btns.forEach(function (btn) {
+    btn.addEventListener('click',
+    function(){
+        if(value<0){
+            val.style.color="red";
+        }
+        if(value>0){
+            val.style.color="green";
+        }
+        if(value==0){
+            val.style.color="black";
+        }
+        val.textContent=value; 
+    });
 });
